f7e7479230024e679f99915b309bc74484cf171b
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-14 14:11:53 +0200

    feat: allow values to be used in fields

--- src/array.js
+++ src/array.js
@@ -1,5 +1,5 @@
 module.exports = types => (key, args, fields = {}) => {
-  const {__proto__, key: _1, types: _2, args: _3, values: _4, ...rest} = fields
+  const {__proto__, key: _1, types: _2, args: _3, ...rest} = fields
 
   return {
     key,
