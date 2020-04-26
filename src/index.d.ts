c242696bb4aee337e5b2bd755faf8e159c145a2b
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-26 22:19:33 +0200

    feat: add typescript type declarations for index

---
+++ src/index.d.ts
@@ -0,0 +1,46 @@
+export {array}       from './array'
+export {arrayPos}    from './arrayPos'
+export {bool}        from './bool'
+export {boolPos}     from './boolPos'
+export {command}     from './command'
+export {complement}  from './decorators/complement'
+export {flag}        from './flag'
+export {number}      from './number'
+export {numberPos}   from './numberPos'
+export {string}      from './string'
+export {stringPos}   from './stringPos'
+export {variadic}    from './variadic'
+export {variadicPos} from './variadicPos'
+
+export interface Opt {
+  args?: string[]
+  contradicts?: string[],
+  defaultValues?: any
+  desc?: string
+  descArg?: string
+  implies?: string[]
+  key?: string
+  only?: any[]
+  opts?: Opt[]
+  required?: boolean
+  reverse?: boolean
+  rules?: (opt?: Opt) => (opts?: Opt[]) => boolean
+  types?: string[]
+  values?: any[]
+  [key: string]: any
+}
+
+export interface Fields {
+  contradicts?: string[],
+  defaultValues?: any
+  desc?: string
+  descArg?: string
+  implies?: string[]
+  only?: any[]
+  opts?: Opt[]
+  required?: boolean
+  reverse?: boolean
+  rules?: (opt?: Opt) => (opts?: Opt[]) => boolean
+  values?: any[]
+  [key: string]: any
+}
