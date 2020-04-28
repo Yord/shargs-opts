07d88769e63d19cd0aba96f6cb07c5ef9a099c9e
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-28 10:35:19 +0200

    feat: change argument order in program

--- src/program.js
+++ src/program.js
@@ -1,6 +1,6 @@
 const {variadicPos} = require('./variadicPos')
 
-const program = (opts = []) => (key, fields = {}) => variadicPos(key, {opts, ...fields})
+const program = (key, opts = [], fields = {}) => variadicPos(key, {opts, ...fields})
 
 module.exports = {
   program
